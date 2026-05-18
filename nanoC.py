4a5cabcd936c83594e4f928c0c405ea87386dde5
Alassane-diaa <dialassane19@gmail.com>
2026-05-18 10:51:45 +0200

    init: first commit

---
+++ nanoC.py
@@ -0,0 +1,158 @@
+import lark
+
+cpt = 0
+
+grammaire = lark.Lark("""
+IDENTIFIER: /[a-zA-Z_][a-zA-Z_0-9]*/
+TYPE: "int"
+OPBIN: /[+\\-*\\/<>%]/
+decl: TYPE IDENTIFIER -> declaration
+expression: IDENTIFIER -> variable 
+          | SIGNED_NUMBER -> entier
+          | expression OPBIN expression -> binaire
+commande: IDENTIFIER "=" expression ";" -> assignation
+        | "print" "(" expression ")" ";" -> print
+        | "if" "(" expression ")" "{" commande "}" -> if
+        | "while" "(" expression ")" "{" commande "}" -> while
+        | (commande)* commande -> sequence
+        | "pass" -> pass
+vars: (IDENTIFIER ",")* IDENTIFIER -> liste_vars
+main: "main" "(" vars ")" "{" commande "return" "(" expression ")" ";" "}"       
+%import common.WS
+%import common.SIGNED_NUMBER
+%ignore WS
+""", start="main")
+
+op2asm = {"+": "add rax, rbx", "-": "sub rax, rbx"}
+
+def asm_expression(e):
+    if e.data == "variable":
+        return f"mov rax, [{e.children[0].value}]"
+    if e.data == "entier":
+        return f"mov rax, {e.children[0].value}"
+    e_left = e.children[0]
+    e_op = e.children[1]
+    e_right = e.children[2]
+    asm_left = asm_expression(e_left)
+    asm_right = asm_expression(e_right)
+    return f"""{asm_left}
+push rax
+{asm_right}
+mov rbx, rax
+pop rax
+{op2asm[e_op.value]}"""
+
+
+def asm_commande(c) -> str:
+    global cpt
+    if c.data == "assignation":
+        var = c.children[0]
+        exp = c.children[1]
+        return f"{asm_expression(exp)}\nmov [{var.value}], rax"
+    elif c.data == "pass":
+        return "nop"
+    elif c.data == "print":
+        return f"""{asm_expression(c.children[0])}
+mov rdi, format
+mov rsi, rax
+xor rax, rax
+call printf
+"""
+    elif c.data == "while":
+        exp = c.children[0]
+        body = c.children[1]
+        idx = cpt
+        cpt += 1
+        return f"""loop{idx}:{asm_expression(exp)}
+cmp rax, 0
+jz end{idx}
+{asm_commande(body)}
+jmp loop{idx}
+end{idx}: nop
+"""
+    elif c.data == "sequence":
+        d = c.children[0]
+        tail = c.children[1]
+        return f"{asm_commande(d)}\n {asm_commande(tail)}"
+    else:
+        return ""
+
+def asm_vars(ast):
+    return "\n".join(
+        f"""
+        mov rbx, [argv]
+        add rbx, {(i+1)*8}
+        mov rdi, [rbx]
+        call atoi
+        mov [{ast.children[i].value}], rax
+        """
+        for i in range(len(ast.children)))
+
+def asm_decls_vars(ast):
+    return "\n".join(f"{ast.children[i].value} : dq 0" 
+                     for i in range(len(ast.children)))
+
+
+def pp_expression(ast) -> str :
+    if ast.data in ("variable", "entier"):
+        return ast.children[0].value
+    elif ast.data == "binaire":
+        eg = pp_expression(ast.children[0])
+        op = ast.children[1].value
+        ed = pp_expression(ast.children[2])
+        return  f"{eg} {op} {ed}"
+    else :
+        return ""
+    
+def pp_commande(ast) -> str:
+    if ast.data == "sequence":
+        cg = pp_commande(ast.children[0])
+        cd = pp_commande(ast.children[1])
+        return f"{cg} \n{cd}"
+    elif ast.data == "assignation":
+        eg = ast.children[0].value
+        ed = pp_expression(ast.children[1])
+        return f"{eg} = {ed} ;"
+    elif ast.data == "print":
+        return f"print({pp_expression(ast.children[0])})"
+    elif ast.data in ("if", "while"):
+        cond = pp_expression(ast.children[0])
+        cmd = pp_commande(ast.children[1])
+        return f"{ast.data} ({cond}) then \n{cmd}"
+    elif ast.data == "pass":
+        return "pass"
+    else: 
+        return ""
+
+def pp_vars(ast) -> str:
+    args = f""
+    for child in ast.children:
+        args += child.value + ","
+    args = args[:-1]
+    return args
+
+def pp_main(ast) -> str:
+    args = pp_vars(ast.children[0])
+    cmd = pp_commande(ast.children[1])
+    ret = pp_expression(ast.children[2])
+    return f"main({args}) {{ \n{cmd} \nreturn {ret} \n}}"
+
+
+def asm_main(ast):
+    decls = asm_decls_vars(ast.children[0])
+    vs = asm_vars(ast.children[0])
+    cmd = asm_commande(ast.children[1])
+    ret = asm_expression(ast.children[2])
+    squelette = open("squelette.asm", "r").read()
+    squelette = squelette.replace("DECL_VARS", decls)
+    squelette = squelette.replace("INIT_VARS", vs)
+    squelette = squelette.replace("COMMAND", cmd)
+    squelette = squelette.replace("RETURN", ret)
+    return squelette
+
+
+if __name__ == "__main__":
+    src = open("source.c", "r").read()
+    t = grammaire.parse(src)
+    with open("resultat.asm", "w") as f:
+        f.write(asm_main(t))
---
+++ source.c
@@ -0,0 +1,8 @@
+main(x, y) {
+while(x){
+    x = x - 1;
+    y = y + 1;
+}
+print(y);
+return (y);
+}
